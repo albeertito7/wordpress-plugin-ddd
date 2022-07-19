d61027c4177f90899993f8a47b0b792ebe4fc6d6
albert <albertperezdatsira@gmail.com>
2022-07-19 19:20:42 +0200

    Update template-cart.php

--- admin/page-templates/template-cart.php
+++ admin/page-templates/template-cart.php
@@ -1,5 +1,8 @@
 <?php
 
+// adds the header built upon the theme builder to the page
+get_header();
+
 ?>
 
 <div id="main-content" style="padding:100px;">
@@ -8,3 +11,13 @@
     <hr />
 
 </div>
+
+<?php
+
+// adds the content built upon the theme builder to the page
+// meaning it will be displayed before any other programmatically adding
+the_content();
+
+?>
+
+<?php get_footer(); ?>
