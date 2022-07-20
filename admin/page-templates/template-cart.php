d99dea223f1cf318e693d43dc5b6fbb13dededb1
albert <albertperezdatsira@gmail.com>
2022-07-20 13:26:30 +0200

    Update template-cart.php

--- admin/page-templates/template-cart.php
+++ admin/page-templates/template-cart.php
@@ -1,5 +1,13 @@
 <?php
 
+$cart = ProductCart::collect();
+
+/*$packages = $cart['Package'];
+$activity = $cart['Activity'];
+$hotels = $cart['Hotel'];*/
+
+$repo = PackageRepository::getInstance();
+
 // adds the header built upon the theme builder to the page
 get_header();
 
@@ -7,8 +15,28 @@ get_header();
 
 <div id="main-content" style="padding:100px;">
 
-    <h2><?php _e('My Cart'); ?></h2>
-    <hr />
+    <?php foreach($cart as $class => $products) { ?>
+
+        <h2><?php _e('My ' . $class); ?></h2>
+        <hr />
+
+        <!-- Packages Grid -->
+        <div class="package-grid" style="margin-top: 30px; display: flex; justify-content: space-evenly; align-self: center; flex-wrap: wrap;">
+
+            <?php foreach($products as $id => $cart_info) {
+
+                $product = $repo->findById($id);
+
+                if ( $product->getStatus() == "publish" ) {
+                    Utils::includeCustom(plugin_dir_path( __FILE__ ) . 'partials/entity-card.php', array(
+                        'package' => $product // cast to product for the entity-card
+                    ));
+                }
+
+            } ?>
+        </div>
+
+    <?php } ?>
 
 </div>
 
